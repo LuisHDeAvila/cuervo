e013ebe1be6239cf65a92db082838b52ff43a945
eleache <IKPEdLzwoD26aKE@gmail.com>
2022-02-03 14:22:55 -0600

    'l'

---
+++ src/MAKE.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+cat header >> ../cuervo
+cat bashart.sh >> ../cuervo
+cat checker >> ../cuervo
+cat redaccionHelp >> ../cuervo  # aun no escribo el help, estoy pensando que variables incluir
+cat cuervorc >> ../cuervo
