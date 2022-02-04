efd83868ced5617013daa580c0a6b6ca70a4e93c
eleache <luishdeavila@github.com>
2022-02-04 00:07:49 -0600

    'ma'

--- src/MAKE.sh
+++ src/MAKE.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-
+rm ../cuervo > /dev/null
 cat header >> ../cuervo
 cat bashart.sh >> ../cuervo
 cat checker >> ../cuervo
 cat redaccionHelp >> ../cuervo  # aun no escribo el help, estoy pensando que variables incluir
 cat cuervorc >> ../cuervo
+cat salir >> ../cuervo
+
+echo '          START TESTING       [ENTER]'
+chmod +x ../cuervo
+../cuervo 
--- src/bashart.sh
+++ src/bashart.sh
@@ -1,8 +1,8 @@
-
-# EFECTO MATRIX
-# MATRIX_STYLE={ perl -e '$|++; while (1) { print " " x (rand(35) + 1 ), int(rand(2)) }' ;                                                                                  
+    
 # ESTILOS
 #  lang ->                   es                  en
+
+
 REDCOLOR="\e[1;31m\033[1m" # rojo         |   red---------- -|> 31
 GREENCOLOR="\e[0;32m\033[1m" # verde        |   green-------- -|> 32
 BLUECOLOR="\e[0;34m\033[1m" # azul         |   blue--------- -|> 34
@@ -29,17 +29,8 @@ ANON
 echo -e "${REDCOLOR}"; ENIGMA | pv -qL 1100 ; echo -e "${_END}" 
 echo '                  press [ENTER] to continue...'
 read INITIALIZATION
-clear;
-
-
-
-                                                    
-                                                    
-                                                    
-                                                    
-                                                    
-                                                    
-                                                    
+clear;                             
+                       
 # SALIDA ESTILIZADA
 trap ctrl_c INT         
 function ctrl_c(){
